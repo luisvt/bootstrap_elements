3b201d895657dc64810aed7242fdeec68a024aa9
luis <luisbigvargas@gmail.com>
2015-02-13 00:23:24 -0500

    add btstrp-button

--- lib/bstrp_button/bstrp_button.dart
+++ lib/bstrp_button/bstrp_button.dart
@@ -1,23 +1,19 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
-
-import 'package:paper_elements/paper_input.dart';
 import 'package:polymer/polymer.dart';
 
-/// A Polymer `<main-app>` element.
-@CustomTag('main-app')
-class MainApp extends PolymerElement {
-  @observable String reversed = '';
-
-  /// Constructor used to create instance of MainApp.
-  MainApp.created() : super.created();
-
-  void reverseText(Event event, Object object, PaperInput target) {
-    reversed = target.value.split('').reversed.join('');
-  }
+/// A Polymer `<bstrp-button>` element.
+@CustomTag('bstrp-button')
+class BstrpButton extends PolymerElement {
+  /// Constructor used to create instance of BstrpButton.
+  BstrpButton.created() : super.created();
   
+  @published String icon;
+  @published String iconPos = 'left';
+  @published String label;
+  @published String tooltip;
+
   // Optional lifecycle methods - uncomment if needed.
 
 //  /// Called when an instance of main-app is inserted into the DOM.
